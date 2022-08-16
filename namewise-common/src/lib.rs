8ddfd113c14f327f8157d9c3de9da23696f28246
Amr Hassan <amr.hassan@gmail.com>
2022-08-16 11:41:01 +0200

    Fix error message not beign rendered correctly

--- namewise-common/src/lib.rs
+++ namewise-common/src/lib.rs
@@ -1,14 +1,27 @@
 use std::convert::Infallible;
 
-#[derive(Debug, strum::Display, thiserror::Error)]
+#[derive(Debug, derive_more::Display, thiserror::Error)]
 pub enum NamewiseError {
-    #[strum(serialize = "_0")]
+    #[display(fmt= "Missing field error: {_0}")]
     MissingField(String),
+    #[display(fmt= "Generic error: {_0}")]
     Generic(Box<dyn std::error::Error + Send + Sync>),
 }
 
 impl From<Infallible> for NamewiseError {
     fn from(_: Infallible) -> Self {
         panic!("An Infallible should never happen")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_error_message() {
+        assert_eq!(
+            NamewiseError::MissingField("x is missing".to_string()).to_string(),
+            "Missing field error: x is missing"
+        );
     }
 }
